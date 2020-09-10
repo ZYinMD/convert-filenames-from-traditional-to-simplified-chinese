28c6881692d85837f92a9d765381d13417fcb112
Zhi Yin <Z.Yin.MD@gmail.com>
2020-09-10 16:29:17 -0400

    basic structure

--- src/rename.ts
+++ src/rename.ts
@@ -1,1 +1,13 @@
-console.log('hello world');
+// import { promises as fs } from 'fs';
+
+main();
+
+function main() {
+	const targetPath = process.argv[2];
+	if (targetPath) processPath(targetPath);
+	else console.error('Please check README.md about syntax');
+}
+
+function processPath(path: string) {
+	console.log('path:\n\t', path);
+}
