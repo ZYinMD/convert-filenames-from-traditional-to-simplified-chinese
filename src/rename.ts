469f44aa0797bc5d29f79d0b82ce6dc2b11bbd0f
Zhi Yin <Z.Yin.MD@gmail.com>
2020-09-11 00:28:34 -0400

    a better mvp

--- src/rename.ts
+++ src/rename.ts
@@ -1,13 +1,17 @@
-// import { promises as fs } from 'fs';
+import { promises as fs } from 'fs';
 
 main();
 
-function main() {
+async function main() {
 	const targetPath = process.argv[2];
-	if (targetPath) processPath(targetPath);
-	else console.error('Please check README.md about syntax');
+	if (targetPath) {
+		return processDir(targetPath);
+	} else console.error('Please check README.md about syntax');
 }
 
-function processPath(path: string) {
-	console.log('path:\n\t', path);
+async function processDir(path: string) {
+	const dir = await fs.opendir(path);
+	for await (const dirent of dir) {
+		console.log('name', dirent.name, 'isFile', dirent.isFile());
+	}
 }
